759838dd3112882a07f28803abcd9a18dd1b2799
Yang-heesu <hsge0602@gmail.com>
2023-01-31 17:38:18 +0900

    todoList에 들어갈 요소 - 텍스트

---
+++ src/todoListItemText.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface TodoListItemTextProps {
+	itemCompleted: boolean;
+	children: React.ReactNode; //태그 사이의 값을 받는데 사용한다.
+}
+
+const TodoListItemText = ({
+	itemCompleted,
+	children
+}: TodoListItemTextProps) => {
+  return (
+    <div className={`itemText ${itemCompleted ? 'completedItemText' : ''}`}>
+      {children}
+    </div>
+  );
+}
+
+export default TodoListItemText;
